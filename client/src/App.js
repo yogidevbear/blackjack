526b96783c1af6b959c267e7395b3341480a2cb3
Andrew Jackson <andrewsyoga@gmail.com>
2023-10-08 14:54:24 +0100

    Switch out reactive rendering for html string literals approach to reduce complexity with OOP code and funcation React code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,191 +1,117 @@
-import { useEffect, useState } from "react";
 import Deck from './models/deck'
+import Hand from "./models/hand";
 import './App.css';
-
-
 
 function App() {
   
-  const [deck, setDeck] = useState()
-  const [player, setPlayer] = useState('player')
-  const [playerHand, setPlayerHand] = useState([])
-  const [dealerHand, setDealerHand] = useState([])
-  const [playerScores, setPlayerScores] = useState({})
-  const [dealerScores, setDealerScores] = useState({})
-  const [winner, setWinner] = useState('')
+  const deck = new Deck()
+  const playerHand = new Hand()
+  const dealerHand = new Hand()
 
   function startGame() {
-    setPlayer('player')
-    setPlayerHand([])
-    setDealerHand([])
-    let tempDeck = new Deck()
-    tempDeck.shuffle()
-    let hand1 = [tempDeck.dealCard()]
-    let hand2 = [tempDeck.dealCard()]
-    hand1 = [...hand1,tempDeck.dealCard()]
-    hand2 = [...hand2,tempDeck.dealCard()]
-    hand2[1].swapVisibility()
-    setPlayerHand(playerHand => [...playerHand,...hand1])
-    setDealerHand(dealerHand => [...dealerHand,...hand2])
-    console.log('dealerHand',dealerHand)
-    setDeck(tempDeck)
-    setWinner('')
+    document.getElementById('winner').innerHTML = ''
+    document.getElementById('dealer-score').innerHTML = ''
+    playerHand.refresh()
+    dealerHand.refresh()
+    deck.refresh()
+    deck.shuffle()
+    playerHand.pickupCard(deck.dealCard())
+    dealerHand.pickupCard(deck.dealCard())
+    playerHand.pickupCard(deck.dealCard())
+    let hiddenCard = deck.dealCard()
+    hiddenCard.swapVisibility()
+    dealerHand.pickupCard(hiddenCard)
+    // console.log('playerHand.cards',playerHand.cards)
+    // console.log('dealerHand.cards',dealerHand.cards)
+    renderDealerHand()
+    renderPlayerHand()
+    renderGameButtons()
+    renderPlayerScore()
   }
 
-  function bestValidScore(scores) {
-    const validScores = Object.values(scores).filter(v => v < 22)
-    return Math.max(...validScores,0)
+  function renderCard(card) {
+    return card.visible ?
+      `<img src="/img/cards/${card.image}" alt="${card.face} of ${card.suit}" id="${card.face}_${card.suit}" />` :
+      `<img key="${card.face}_${card.suit}" src="/img/cards/back_blue.png" alt="Dealer hole card" />`
   }
 
-  useEffect(
-    () => {
-      setPlayerScores(playerScores => calcHandScores(playerHand))
-      if (playerHand.length === 5) {
-        setPlayer('dealer')
-      }
-    },
-    [playerHand]
-  )
+  function renderCards(cards) {
+    // console.log('cards in hand',cards)
+    return `<div class="hand">${cards.map((card) => renderCard(card)).join('')}</div>`
+  }
 
-  useEffect(
-    () => {
-      setDealerScores(dealerScores => calcHandScores(dealerHand))
-      // if (player === 'dealer' && Math.max(Object.values(dealerScores).filter(v => v < 22)) < 17) {
-      //   setDealerHand(dealerHand => [...dealerHand,deck.dealCard()])
-      // }
-    },
-    [dealerHand]
-  )
+  function renderDealerHand() {
+    document.getElementById('App-dealer').innerHTML = renderCards(dealerHand.cards)
+  }
 
-  useEffect(
-    () => {
-      console.log('playerScores',playerScores)
-      if (playerScores.minScore > 21) {
-        setWinner('Dealer')
-      }
-    },
-    [playerScores]
-  )
+  function renderPlayerHand() {
+    document.getElementById('App-player').innerHTML = renderCards(playerHand.cards)
+  }
 
-  useEffect(
-    () => {
-      console.log('dealerScores',dealerScores)
-      if (dealerScores.minScore > 21) {
-        setWinner('You')
-      }
-    },
-    [dealerScores]
-  )
+  function renderGameButtons() {
+    document.getElementById('game-buttons').hidden = false
+    document.getElementById('start-game-button').hidden = true
+  }
 
-  useEffect(
-    () => {
-      if (player === 'dealer') {
-        let playerFinalScore = Object.values(playerScores).filter(v => v < 22)
-        console.log('playerFinalScore',playerFinalScore)
-        console.log('Math.max([17,...playerFinalScore])',Math.max(17,...playerFinalScore))
-        if (dealerScores.maxScore < Math.max(...playerFinalScore,17)) {
-          setDealerHand(dealerHand => [...dealerHand,deck.dealCard()])
-        }
-      }
-    },
-    [player]
-  )
+  function renderPlayerScore() {
+    const score = playerHand.isBust() ? 'Bust!' : playerHand.bestValidScore()
+    document.getElementById('player-score').innerHTML = `Your score: ${score}`
+  }
 
-  function calcHandScores(hand) {
-    const scores = {
-      minScore: 0,
-      maxScore: 0
-    }
-    for (let {score} of hand) {
-      scores.minScore += score[0]
-      scores.maxScore += score.length > 1 ? score[1] : score[0]
-    }
-    return scores
+  function renderDealerScore() {
+    const score = dealerHand.isBust() ? 'Bust!' : dealerHand.bestValidScore()
+    document.getElementById('dealer-score').innerHTML = `Dealer's score: ${score}`
+  }
+
+  function renderWinner() {
+    const dealerScore = dealerHand.bestValidScore()
+    const playerScore = playerHand.bestValidScore()
+    document.getElementById('winner').innerHTML = dealerScore === playerScore ? 'Draw' : dealerScore > playerScore ? 'Dealer wins!' : 'You win!'
   }
 
   const handleDeal = () => {
-    setPlayerHand(playerHand => [...playerHand,deck.dealCard()])
-  }
-
-  // const handleDealerHands = () => {
-  //   console.log('inside handleDealerHands()')
-  //   if (bestValidScore(dealerScores) < 17) {
-  //     console.log('min not reached...')
-  //     setDealerHand(dealerHand => [...dealerHand,deck.dealCard()])
-  //     console.log('recall handleDealerHands()')
-  //     handleDealerHands()
-  //   }
-  //   (bestValidScore(dealerScores) > bestValidScore(playerScores)) ? setWinner('Dealer') : setWinner('You')
-  // }
-
-  const handleHold = () => {
-    // setDealerHand(dealerHand => [dealerHand[0],{...dealerHand[1],hidden:false}])
-    // console.log('card 2',dealerHand[1])
-    const showHand = [...dealerHand]
-    showHand[1].swapVisibility()
-    setDealerHand(dealerHand => showHand)
-    setPlayer('dealer')
-    // handleDealerHands() 
-  }
-
-  const displayScores = (scoresState) => {
-    if (scoresState.minScore !== scoresState.maxScore && scoresState.maxScore > 21)
-      return scoresState.minScore
-    let scores = `${scoresState.maxScore}`
-    if (scoresState.minScore !== scoresState.maxScore) {
-      scores += `(${scoresState.minScore})`
+    const newCard = deck.dealCard()
+    // console.log('new card', newCard)
+    playerHand.pickupCard(newCard)
+    // console.log('playerHand.cards',playerHand.cards)
+    renderPlayerHand()
+    renderPlayerScore()
+    if (playerHand.isBust()) {
+      document.getElementById('game-buttons').hidden = true
+      document.getElementById('start-game-button').hidden = false
+    } else if (playerHand.cards.length === 5) {
+      handleStand()
     }
-    return scores
+  }
+
+  const handleStand = () => {
+    document.getElementById('game-buttons').hidden = true
+    dealerHand.cards[1].swapVisibility()
+    while (!dealerHand.isBust() && dealerHand.cards.length < 5 && dealerHand.bestValidScore() < 17) {
+      dealerHand.pickupCard(deck.dealCard())
+    }
+    renderDealerHand()
+    renderDealerScore()
+    renderWinner()
+    document.getElementById('start-game-button').hidden = false
   }
 
   return (
     <div className="App">
-      <div id="App-dealer">
-        {dealerHand.length > 0 &&
-          <div className="hand">
-            {console.log('dealerHand',dealerHand)}
-            {dealerHand.map((card,idx) => {
-              if (card.visible) return (<img key={`${card.face}_${card.suit}`} src={`/img/cards/${card.image}`} alt={`${card.face} of ${card.suit}`} id={`${card.face}_${card.suit}`} />)
-              return (<img key={`${card.face}_${card.suit}`} src="/img/cards/back_blue.png" alt="Dealer hole card" />)
-            })}
-          </div>
-        }
-      </div>
+      <div id="App-dealer"></div>
       <div id="App-center">
-        {player === 'dealer' &&
-          <div>Dealer's score: {displayScores(dealerScores)}</div>
-        }
-        <h1>
-          Blackjack &#9824;
-          {winner &&
-            <span className="winner">{winner} wins!</span>
-          }
-        </h1>
+        <div id="dealer-score"></div>
+        <h1>Blackjack &#9824; <span id="winner"></span></h1>
         <div>
-          {(!playerHand.length || winner) &&
-            <button onClick={startGame}>Start game!</button>
-          }
-          {(playerHand.length > 0 && !winner) &&
-            <span>
-              {player !== 'dealer' &&
-                <span>
-                  <button onClick={handleDeal}>Hit</button>
-                  <button onClick={handleHold}>Hold</button>
-                </span>
-              }
-              <span className="score">Your score: {displayScores(playerScores)}</span>
-            </span>
-          }
+          <button id="start-game-button" onClick={startGame}>Start game!</button>
+          <span id="game-buttons" hidden={true}>
+            <button onClick={handleDeal}>Hit</button>
+            <button onClick={handleStand}>Stand</button>
+          </span>
+          <span id="player-score" className="score"></span>
         </div>
       </div>
-      <div id="App-player">
-        {playerHand.length > 0 &&
-          <div className="hand">
-            {console.log('playerHand',playerHand)}
-            {playerHand.map((card,idx) => <img key={`${card.face}_${card.suit}`} src={`/img/cards/${card.image}`} alt={`${card.face} of ${card.suit}`} id={`${card.face}_${card.suit}`} />)}
-          </div>
-        }
-      </div>
+      <div id="App-player"></div>
     </div>
   );
 }
--- client/src/models/deck.js
+++ client/src/models/deck.js
@@ -1,10 +1,38 @@
 import Card from './card.js'
 
+const suits = ['hearts','spades','clubs','diamonds']
+const faces = ["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
+
 export default class Deck {
   constructor() {
     this.deck = []
+    this.refresh = refresh
     this.shuffle = shuffle
     this.dealCard = dealCard
+
+    function refresh() {
+      this.deck = []
+      for (let suit of suits) {
+        for (let face of faces) {
+          const score = []
+          switch (face) {
+            case "ace":
+              score.push(1)
+              score.push(11)
+              break
+            case "king":
+            case "queen":
+            case "jack":
+              score.push(10)
+              break
+            default:
+              score.push(parseInt(face))
+          }
+          this.deck.push(new Card({face:face,suit:suit,score:score}))
+        }
+      }
+      // console.log('deck refreshed:', this.deck)
+    }
 
     function shuffle() {
       const { deck } = this
@@ -21,31 +49,9 @@ export default class Deck {
       return this
     }
 
-    const suits = ['hearts','spades','clubs','diamonds']
-    const faces = ["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
-    for (let suit of suits) {
-      for (let face of faces) {
-        const score = []
-        switch (face) {
-          case "ace":
-            score.push(1)
-            score.push(11)
-            break
-          case "king":
-          case "queen":
-          case "jack":
-            score.push(10)
-            break
-          default:
-            score.push(parseInt(face))
-        }
-        this.deck.push(new Card({face:face,suit:suit,score:score}))
-      }
-    }
-
     function dealCard() {
-      const { deck } = this
-      return deck.pop()
+      // console.log('the deck before deal:', this.deck)
+      return this.deck.pop()
     }
   }
 }
